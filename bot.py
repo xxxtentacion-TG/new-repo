6c2f1f4e19474e62bcd29ad71798497707a95caf
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-08-05 14:03:11 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+bot = Client(
+  "message-bot",
+  bot_token="1943952420:AAG4yAPtx_vlFSoXa7yzDhe4xcWKenTEfMA",
+  api_id="3020564",
+  api_hash="91c026fadfdc442f504a0bd3e5c8cd18",
+  )
+
+@bot.on_message(filters.command(['start']))
+def start(Client, messages):
+  messages.reply(f"hello {message.from_user.first_name} How are You",
+  reply_markup = InlineKeyboardMarkup( # button
+      [
+         [
+            InlineKeyboardButton('Owner', url='https://t.me/XXXTENTACION_OF_TG'),
+            InlineKeyboardButton('group', url='https://t.me/MGMOVIEGRAM')
+         ],
+         [
+            InlineKeyboardButton('help', callback_data="help"),
+         ]
+      ]
+      )
+     )
+     
+     bot.run()
